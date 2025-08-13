9c5e67205c88606df73e2e60cd1cfbe740778191
ahlaiptek <iptekahla@gmail.com>
2025-08-13 22:28:36 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/20250813-portal-laporan-warga/',
   plugins: [react(), tailwindcss()],
 })
 
